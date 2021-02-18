5774215800d47427ce7fc992c3e654f95bf2cea8
tmahone5 <mahoneandson55@aol.com>
2021-02-17 22:24:31 -0600

    Creating IAM users and groups

---
+++ createusr.sh
@@ -0,0 +1,21 @@
+#1/bin/bash
+
+#creating this script tp create IAM user in AWS
+# creating enviromental vairable. When script is ran this will be the first argument
+#this is will be the group name.
+group=$1
+# Policy for group
+#vpolicy=$2
+# user name
+usrname=$2
+#usrname=$3
+# AWS CLI commands
+aws iam create-group --group-name $group
+#aws iam list-attached-group-policies --group-name $policy
+aws iam create-user --user-name $usrname
+aws iam add-user-to-group --user-name $usrname --group-name $group
+clear
+aws iam get-group --group-name $group
+
+
+
